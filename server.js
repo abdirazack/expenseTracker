c7c18d988799c59da3d60b05b95f696fd69914b6
eng-omarmo <omarmoo683@gmail.com>
2024-03-10 10:27:59 +0300

    server up and running

--- server.js
+++ server.js
@@ -1,32 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
-dotenv.config();
-
-
-const path = require("path");
-
-const connectToDatabase = require('./config/db')
-
 const ejs = require("ejs");
 
 dotenv.config();
 
 const app = express();
 
-connectToDatabase();
-
 app.set("view engine", "ejs");
-
-app.set("views", path.join(__dirname, "src", "views"));
-
-app.use(express.json())
-
-// Middleware to parse URL-encoded form data
-app.use(express.urlencoded({ extended: true }));
-
-const publicPath = path.join(__dirname, "public");
-
-app.use(express.static(publicPath));
 
 const port = process.env.PORT || 3000;
 
@@ -34,8 +14,6 @@ app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
 
-app.get("/", (req, res) => {
-  res.render("auth/login");
+app.get("/home", (req, res) => {
+  res.render("home");
 });
-
-app.use('/users',require('./src/routes/UserRoute'))
