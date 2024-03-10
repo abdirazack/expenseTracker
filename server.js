7920d5356d188790febcc1ac2ca9c68b3f8e04d6
eng-omarmo <omarmoo683@gmail.com>
2024-03-10 11:27:55 +0300

    updated config filrd

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require("express");
 
 const dotenv = require("dotenv");
+
+const path = require("path");
+
+const connectToDatabase = require('./config/db')
+
 const ejs = require("ejs");
 
 dotenv.config();
 
 const app = express();
 
+connectToDatabase();
+
+app.set("views", path.join(__dirname, "src", "views"));
+
 app.set("view engine", "ejs");
+
+const publicPath = path.join(__dirname, "public");
+
+app.use(express.static(publicPath));
 
 const port = process.env.PORT || 3000;
 
